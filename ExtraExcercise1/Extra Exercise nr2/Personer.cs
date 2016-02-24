70b0be3845a17dec4e78eb8a97c32ac62a1acdd1
scoophamp <hampus_cousin@hotmailcom>
2016-02-24 14:49:52 +0100

    La till en klass med get namn

---
+++ ExtraExcercise1/Extra Exercise nr2/Personer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_Exercise_nr2
+{
+    class Personer
+    {
+        private string firstName;
+
+        public string firstname
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        private string lastName;
+
+        public string lastname
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        private string fullname;
+
+        public string MyProperty
+        {
+            get { return firstname + lastName; }
+            set { fullname = value; }
+        }
+
+    }
+}
