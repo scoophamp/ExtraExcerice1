6a0ac54d263dc926385670038f96a8a89faa5411
ThereseSjogren <nike_8@hotmail.com>
2016-02-24 12:05:05 +0100

    La till i metoden

--- ExtraExcercise1/Extra Exercise nr2/Program.cs
+++ ExtraExcercise1/Extra Exercise nr2/Program.cs
@@ -10,7 +10,16 @@ namespace Extra_Exercise_nr2
     {
         static void TaBortFilter()
         {
-           
+            string text = "    Hello H e l s i n g b o r g    !";
+            string[] nytext = text.Split(new char[] { ' ' });
+
+            var result = string.Empty;
+            foreach (var c in nytext)
+            {
+                if (c != " ")
+                    result += c;
+            }
+
         }
         static void Main(string[] args)
         {
